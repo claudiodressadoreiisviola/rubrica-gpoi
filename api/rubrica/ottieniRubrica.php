0d43ecd7c1bf0db0ba8278d3f01d84d9084e5f75
lucamedea <99017895+lucamedea@users.noreply.github.com>
2023-04-30 16:53:29 +0200

    api fatte
    mancano quelle della sessione che non so come vadano gestite

---
+++ api/rubrica/ottieniRubrica.php
@@ -0,0 +1,39 @@
+<?php
+
+// Carico gli script di base
+spl_autoload_register(function ($class) {
+    require __DIR__ . "/../../common/$class.php";
+});
+
+// Importo la classe Sessione
+require __DIR__ . "/../../model/rubrica.php";
+
+set_exception_handler("errorHandler::handleException");
+set_error_handler("errorHandler::handleError");
+
+header("Content-type: application/json; charset=UTF-8");
+
+if (!isset($_GET['id']) || empty($id = $_GET['id']))
+{
+    echo json_encode(array("Message" => "No id passed"));
+    die();
+}
+
+$rubrica = new Rubrica();
+//qui result è un array
+$result = $rubrica->ottieniRubrica($id);
+//adesso result è un oggetto
+$result = (json_decode(json_encode($result)));
+
+if ((int)$result->id >0)
+{
+
+    echo json_encode($result, JSON_PRETTY_PRINT);
+    die();
+}
+else
+{
+    echo json_encode(array("Message" => "No record"));
+    die();
+}
+?>
