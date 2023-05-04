371fea215e3de7a11d58bb4b7a7eb50db5e4ce71
claudiodressadoreiisviola <dressadore.claudio@iisviolamarchesini.edu.it>
2023-05-04 15:52:41 +0200

    Correzione errorHandler e rimozione routes

--- common/errorHandler.php
+++ common/errorHandler.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . "connect.php";
+require __DIR__ . "/connect.php";
 
 class ErrorHandler
 {
@@ -16,22 +16,7 @@ class ErrorHandler
 
     public static function handleException(Throwable $e): void
     {
-        $sql = "INSERT INTO `exception` ( code, message, file, line )
-        VALUES ( :code, :message, :file, :line )";
-
-        $stmt = $this->conn->prepare($query);
-        $stmt->bindValue(':code', $e->getCode(), PDO::PARAM_INT);
-        $stmt->bindValue(':message', $e->getMessage(), PDO::PARAM_STR);
-        $stmt->bindValue(':file', $e->getFile(), PDO::PARAM_STR);
-        $stmt->bindValue(':line', $e->getLine(), PDO::PARAM_INT);
-
-        $stmt->execute();
-
-        $errorid = $this->conn->lastInsertId();
-
-        http_response_code(500);
-        echo json_encode(array("message"=>"Errore durante l'elaborazione della sua richiesta, per favore comunichi all'amministratore questo codice: {$errorid}"));
-        die();
+        echo json_encode($e);
     }
 
     public static function handleError(int $errno, string $errstr, string $errfile, int $errline): bool
